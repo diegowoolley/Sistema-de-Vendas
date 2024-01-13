bba5f502fddee36c3d4f88d635c4cb66273c29e3
diegowoolley <154614603+diegowoolley@users.noreply.github.com>
2024-01-13 17:30:46 -0300

    Add files via upload

---
+++ Cadastros/cad_funcionarios.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Sistema_de_Vendas
+{
+    public partial class frmcad_funcionarios : Form
+    {
+        public frmcad_funcionarios()
+        {
+            InitializeComponent();
+        }
+    }
+}
