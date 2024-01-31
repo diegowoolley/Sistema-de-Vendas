6bfce5534d7043c36bc7e9fe5e43de98cbe6a447
diegowoolley <154614603+diegowoolley@users.noreply.github.com>
2024-01-31 00:18:45 -0300

    Add files via upload

---
+++ Manutencao/FrmbackupBD.Designer.cs
@@ -0,0 +1,77 @@
+﻿namespace Sistema_de_Vendas
+{
+    partial class FrmbackupBD
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnbackup = new System.Windows.Forms.Button();
+            this.btnrestaurar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnbackup
+            // 
+            this.btnbackup.Location = new System.Drawing.Point(1, 2);
+            this.btnbackup.Name = "btnbackup";
+            this.btnbackup.Size = new System.Drawing.Size(284, 99);
+            this.btnbackup.TabIndex = 0;
+            this.btnbackup.Text = "Backup Banco de dados";
+            this.btnbackup.UseVisualStyleBackColor = true;
+            this.btnbackup.Click += new System.EventHandler(this.btnbackup_Click);
+            // 
+            // btnrestaurar
+            // 
+            this.btnrestaurar.Location = new System.Drawing.Point(1, 104);
+            this.btnrestaurar.Name = "btnrestaurar";
+            this.btnrestaurar.Size = new System.Drawing.Size(284, 108);
+            this.btnrestaurar.TabIndex = 1;
+            this.btnrestaurar.Text = "Restaurar banco de dados";
+            this.btnrestaurar.UseVisualStyleBackColor = true;
+            this.btnrestaurar.Click += new System.EventHandler(this.btnrestaurar_Click);
+            // 
+            // FrmbackupBD
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(289, 217);
+            this.Controls.Add(this.btnrestaurar);
+            this.Controls.Add(this.btnbackup);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "FrmbackupBD";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Backup \\ Restatauração BD";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnbackup;
+        private System.Windows.Forms.Button btnrestaurar;
+    }
+}
---
+++ Manutencao/FrmbackupBD.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Sistema_de_Vendas
+{
+    public partial class FrmbackupBD : Form
+    {
+        public FrmbackupBD()
+        {
+            InitializeComponent();
+        }
+
+        private void btnbackup_Click(object sender, EventArgs e)
+        {
+            
+            SaveFileDialog saveFileDialog = new SaveFileDialog
+            {
+                Filter = "Arquivo SQL (*.sql)|*.sql",
+                Title = "Salvar Backup",
+                FileName = DateTime.Now.ToString("yyyyMMdd_HHmmss") +"backup.sql" 
+            };
+
+           
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                
+                string caminhoBackup = saveFileDialog.FileName;
+
+                
+                string comando = $"mysqldump --user={"root"} --password={""} --host={"localhost"} --databases {"dbsistema_vendas"} > \"{caminhoBackup}\"";
+
+                
+                ProcessStartInfo psi = new ProcessStartInfo("cmd.exe")
+                {
+                    RedirectStandardInput = true,
+                    RedirectStandardOutput = true,
+                    RedirectStandardError = true,
+                    CreateNoWindow = true,
+                    UseShellExecute = false,
+                    WorkingDirectory = "C:\\xampp\\mysql\\bin"
+                };
+
+                Process processo = new Process { StartInfo = psi };
+
+                try
+                {
+                    processo.Start();                    
+                    processo.StandardInput.WriteLine(comando);
+                    processo.StandardInput.Close();
+                    
+                    processo.WaitForExit();
+
+                    MessageBox.Show("Backup concluído com sucesso!");
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Erro ao realizar backup: {ex.Message}");
+                }
+
+            }
+        }
+
+        private void btnrestaurar_Click(object sender, EventArgs e)
+        {
+
+            OpenFileDialog openFileDialog = new OpenFileDialog
+            {
+                Filter = "Arquivo SQL (*.sql)|*.sql",
+                Title = "Selecionar Backup"
+            };
+
+           
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+               
+                string caminhoBackup = openFileDialog.FileName;
+
+                
+                DialogResult resultado = MessageBox.Show("Tem certeza que deseja restaurar o banco de dados? Isso substituirá todos os dados existentes.", "Confirmação", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+
+                
+                if (resultado == DialogResult.Yes)
+                {
+                   
+                    string comando = $"mysql --user={"root"} --password={""} --host={"localhost"} {"dbsistema_vendas"} < \"{caminhoBackup}\"";
+
+                    
+
+                    try
+                    {
+                       
+
+                        MessageBox.Show("Restauração concluída com sucesso!");
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show($"Erro ao realizar restauração: {ex.Message}");
+                    }
+                }
+            }
+
+        }
+    }
+}
