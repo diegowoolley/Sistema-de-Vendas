83faca29d2faba0911713c7a03cc57ddc163aa7f
diegowoolley <154614603+diegowoolley@users.noreply.github.com>
2024-02-02 11:46:54 -0300

    Add files via upload

---
+++ Transacoes/inventario.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace Sistema_de_Vendas
+{
+    partial class inventario
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.lblpesquisar = new System.Windows.Forms.Label();
+            this.txtpesquisa = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.EditMode = System.Windows.Forms.DataGridViewEditMode.EditProgrammatically;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 45);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.Size = new System.Drawing.Size(830, 401);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // lblpesquisar
+            // 
+            this.lblpesquisar.AutoSize = true;
+            this.lblpesquisar.Location = new System.Drawing.Point(1, 22);
+            this.lblpesquisar.Name = "lblpesquisar";
+            this.lblpesquisar.Size = new System.Drawing.Size(96, 13);
+            this.lblpesquisar.TabIndex = 1;
+            this.lblpesquisar.Text = "Pesquisar Produto:";
+            // 
+            // txtpesquisa
+            // 
+            this.txtpesquisa.CharacterCasing = System.Windows.Forms.CharacterCasing.Upper;
+            this.txtpesquisa.Location = new System.Drawing.Point(98, 19);
+            this.txtpesquisa.MaxLength = 80;
+            this.txtpesquisa.Name = "txtpesquisa";
+            this.txtpesquisa.Size = new System.Drawing.Size(331, 20);
+            this.txtpesquisa.TabIndex = 1;
+            this.txtpesquisa.TextChanged += new System.EventHandler(this.txtpesquisa_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.Control;
+            this.label1.ForeColor = System.Drawing.Color.Red;
+            this.label1.Location = new System.Drawing.Point(434, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(383, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Digite o código, nome ou use o leitor de código de barras para buscar o produto";
+            // 
+            // inventario
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(830, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtpesquisa);
+            this.Controls.Add(this.lblpesquisar);
+            this.Controls.Add(this.dataGridView1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "inventario";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Inventário";
+            this.Load += new System.EventHandler(this.inventario_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label lblpesquisar;
+        private System.Windows.Forms.TextBox txtpesquisa;
+        private System.Windows.Forms.Label label1;
+    }
+}
---
+++ Transacoes/inventario.cs
@@ -0,0 +1,102 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Sistema_de_Vendas
+{
+    public partial class inventario : Form
+    {
+        public inventario()
+        {
+            InitializeComponent();
+        }
+
+        conn con = new conn();
+        string sql;
+        MySqlCommand cmd;       
+        
+        private void Listar()
+        {
+            con.AbrirConexao();
+            sql = "SELECT * FROM cad_produtos ORDER BY nome_produto ASC";
+            cmd = new MySqlCommand(sql, con.con);
+            MySqlDataAdapter da = new MySqlDataAdapter();
+            da.SelectCommand = cmd;
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            dataGridView1.DataSource = dt;
+            con.FecharConexao();
+        }
+
+        private void formatargrid()
+        {
+            dataGridView1.Columns[0].HeaderText = "Cód. Produto";
+            dataGridView1.Columns[1].HeaderText = "Descrição";
+            dataGridView1.Columns[2].HeaderText = "Categoria";
+            dataGridView1.Columns[3].HeaderText = "Unidade de Medida";
+            dataGridView1.Columns[4].HeaderText = "Quantidade";
+            dataGridView1.Columns[5].HeaderText = "Fornecedor";
+            dataGridView1.Columns[6].HeaderText = "Peso Médio";
+            dataGridView1.Columns[7].HeaderText = "Peso Bruto";
+            dataGridView1.Columns[8].HeaderText = "Fabricante";
+            dataGridView1.Columns[9].HeaderText = "Valor Compra";
+            dataGridView1.Columns[10].HeaderText = "Valor Venda";
+            dataGridView1.Columns[11].HeaderText = "Margem de Lucro";
+            dataGridView1.Columns[12].HeaderText = "Situação Tributária";
+            dataGridView1.Columns[13].HeaderText = "Aliquota IPI";
+            dataGridView1.Columns[14].HeaderText = "Código IPI";
+            dataGridView1.Columns[15].HeaderText = "Foto";
+            dataGridView1.Columns[16].HeaderText = "Estoque Mínimo";
+            dataGridView1.Columns[17].HeaderText = "Estoque Máximo";
+            dataGridView1.Columns[18].HeaderText = "Validade";
+            dataGridView1.Columns[19].HeaderText = "Etiqueta";
+            dataGridView1.Columns[20].HeaderText = "Adicionado por";
+            dataGridView1.Columns[21].HeaderText = "Data e hora da inclusão";
+
+
+
+
+        }
+
+
+        private void Buscar()
+        {
+            string pesquisa = txtpesquisa.Text;
+            con.AbrirConexao();
+            sql = "SELECT * FROM cad_produtos WHERE nome_produto LIKE @nome or cod_produto LIKE @cod_produto or etiqueta LIKE @etiqueta";
+            cmd = new MySqlCommand(sql, con.con);
+            cmd.Parameters.AddWithValue("@nome", "%" + pesquisa + "%");
+            cmd.Parameters.AddWithValue("@cod_produto", "%" + pesquisa + "%");
+            cmd.Parameters.AddWithValue("@etiqueta", "%" + pesquisa + "%");
+            MySqlDataAdapter da = new MySqlDataAdapter();
+            da.SelectCommand = cmd;
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            dataGridView1.DataSource = dt;
+            con.FecharConexao();
+
+            
+
+
+        }
+
+        private void inventario_Load(object sender, EventArgs e)
+        {
+            Listar();
+            formatargrid();
+        }
+
+        private void txtpesquisa_TextChanged(object sender, EventArgs e)
+        {
+            Buscar();
+            
+        }
+    }
+}
