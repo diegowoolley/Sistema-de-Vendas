1dad3476e131a6c372a6d753778d875fda7472d5
diegowoolley <154614603+diegowoolley@users.noreply.github.com>
2024-02-04 19:19:51 -0300

    Add files via upload

--- funcoes.cs
+++ funcoes.cs
@@ -4,7 +4,9 @@ using System.Collections.Generic;
 using System.Data;
 using System.Drawing;
 using System.Globalization;
+using System.IO;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
@@ -17,6 +19,8 @@ namespace Sistema_de_Vendas
     {
         public static string conectado ;
         public string numero { get; set; }
+
+        private static readonly string chaveCriptografia = "Nsx-sz21";
 
 
         public static void DecNumber(object sender, KeyPressEventArgs e)
@@ -132,9 +136,56 @@ namespace Sistema_de_Vendas
 
 
 
+        }        
+
+        public static string Criptografar(string texto)
+        {
+            using (Aes aesAlg = Aes.Create())
+            {
+                aesAlg.Key = Encoding.UTF8.GetBytes(chaveCriptografia);
+                aesAlg.IV = new byte[aesAlg.BlockSize / 8];
+
+                ICryptoTransform encryptor = aesAlg.CreateEncryptor(aesAlg.Key, aesAlg.IV);
+
+                using (MemoryStream msEncrypt = new MemoryStream())
+                {
+                    using (CryptoStream csEncrypt = new CryptoStream(msEncrypt, encryptor, CryptoStreamMode.Write))
+                    {
+                        using (StreamWriter swEncrypt = new StreamWriter(csEncrypt))
+                        {
+                            swEncrypt.Write(texto);
+                        }
+                    }
+                    return Convert.ToBase64String(msEncrypt.ToArray());
+                }
+            }
         }
 
-       
+        public static string Descriptografar(string textoCriptografado)
+        {
+            using (Aes aesAlg = Aes.Create())
+            {
+                aesAlg.Key = Encoding.UTF8.GetBytes(chaveCriptografia);
+                aesAlg.IV = new byte[aesAlg.BlockSize / 8];
+
+                ICryptoTransform decryptor = aesAlg.CreateDecryptor(aesAlg.Key, aesAlg.IV);
+
+                using (MemoryStream msDecrypt = new MemoryStream(Convert.FromBase64String(textoCriptografado)))
+                {
+                    using (CryptoStream csDecrypt = new CryptoStream(msDecrypt, decryptor, CryptoStreamMode.Read))
+                    {
+                        using (StreamReader srDecrypt = new StreamReader(csDecrypt))
+                        {
+                            return srDecrypt.ReadToEnd();
+                        }
+                    }
+                }
+            }
+        }
     }
-    
+
+
+
 }
+    
+
