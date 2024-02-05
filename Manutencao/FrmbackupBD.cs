8ee0e10c8f4d74f9122b511fa4a2da0520f84b24
diegowoolley <154614603+diegowoolley@users.noreply.github.com>
2024-02-04 23:29:31 -0300

    Add files via upload

--- Manutencao/FrmbackupBD.cs
+++ Manutencao/FrmbackupBD.cs
@@ -74,47 +74,49 @@ namespace Sistema_de_Vendas
 
         private void btnrestaurar_Click(object sender, EventArgs e)
         {
-            if (MessageBox.Show(funcoes.conectado + ", deseja realizar o backUp total do Sistema? ", "Cópia de segurança", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
-            {
-
-                SaveFileDialog saveFileDialog = new SaveFileDialog
+           
+            
+                if (MessageBox.Show(funcoes.conectado + ", deseja realizar a restauração total do Sistema? ao aceitar lembre-se que todos os dados antigos serão substituidos!", "Cópia de segurança", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
                 {
-                    Filter = "Arquivo SQL (*.sql)|*.sql",
-                    Title = "Salvar Backup",
-                    FileName = DateTime.Now.ToString("yyyyMMdd_HHmmss") + "backup.sql"
-                };
-
-
-                if (saveFileDialog.ShowDialog() == DialogResult.OK)
-                {
-
-                    string path = saveFileDialog.FileName;
-
-                    // Specify the directory you want to manipulate.
-
-                    string constring = conn.connec;
-                    // Important Additional Connection Options
-                    constring += "charset=utf8;convertzerodatetime=true;";
-                    string data = DateTime.Now.ToString("dd-MM-yyyy-ss");
-                    string file = path + "backup-" + data + ".sql"; //caminho... optei criar uma pasta em C chamada Backup (nome 'backup'+data)
-                    using (MySqlConnection conn = new MySqlConnection(constring))
+                    OpenFileDialog openFileDialog = new OpenFileDialog
                     {
-                        using (MySqlCommand cmd = new MySqlCommand())
+                        Filter = "Arquivo SQL (*.sql)|*.sql",
+                        Title = "Selecione o Arquivo de Backup"
+                    };
+
+                    if (openFileDialog.ShowDialog() == DialogResult.OK)
+                    {
+                        string filePath = openFileDialog.FileName;
+
+                        string constring = conn.connec;
+                        // Important Additional Connection Options
+                        constring += "charset=utf8;convertzerodatetime=true;";
+
+                        using (MySqlConnection conn = new MySqlConnection(constring))
                         {
-                            using (MySqlBackup mb = new MySqlBackup(cmd)) //instalar o pacote NuGet MySqlBackup.NET
+                            using (MySqlCommand cmd = new MySqlCommand())
                             {
-                                cmd.Connection = conn;
-                                conn.Open();
-                                mb.ImportFromFile(file);
-                                conn.Close();
-                                MessageBox.Show("Backup Realizado com Sucesso!", "Backup Banco de Dados");
+                                using (MySqlBackup mb = new MySqlBackup(cmd))
+                                {
+                                    try
+                                    {
+                                        cmd.Connection = conn;
+                                        conn.Open();
+                                        mb.ImportFromFile(filePath);
+                                        conn.Close();
+                                        MessageBox.Show("Restauração Realizada com Sucesso!", "Backup Banco de Dados");
+                                    }
+                                    catch (Exception ex)
+                                    {
+                                        MessageBox.Show("Erro durante a restauração: " + ex.Message, "Erro");
+                                    }
+                                }
                             }
                         }
                     }
                 }
+            
 
-
-            }
         }
     }
 }
