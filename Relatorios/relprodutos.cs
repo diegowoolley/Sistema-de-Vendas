e25b8f9824ad0bd7e15956ef9ebd41307f293b60
diegowoolley <154614603+diegowoolley@users.noreply.github.com>
2024-02-15 13:34:53 -0300

    Add files via upload

---
+++ Relatorios/relprodutos.cs
@@ -0,0 +1,58 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Sistema_de_Vendas.Relatorios
+{
+    public partial class relprodutos : Form
+    {
+        public relprodutos()
+        {
+            InitializeComponent();
+        }
+        
+        conn con = new conn();
+        string sql;        
+        MySqlCommand cmd;
+        private void relprodutos_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                con.AbrirConexao();
+                sql = "SELECT * FROM cad_produtos";
+                cmd = new MySqlCommand(sql, con.con);
+                MySqlDataAdapter da = new MySqlDataAdapter();
+                da.SelectCommand = cmd;
+
+                DataTable dt = new DataTable();
+                da.Fill(dt);
+
+                con.FecharConexao();
+
+                this.reportViewer1.LocalReport.DataSources.Clear();
+                this.reportViewer1.LocalReport.DataSources.Add(new Microsoft.Reporting.WinForms.ReportDataSource("DataSet1", dt));
+
+                this.reportViewer1.RefreshReport();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Erro: " + ex.Message);
+            }
+
+
+
+
+
+
+        }
+
+       
+    }
+}
