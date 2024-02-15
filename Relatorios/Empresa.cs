53042aa82ac80f01a6fd3cc6dadc44d89f7621d9
diegowoolley <154614603+diegowoolley@users.noreply.github.com>
2024-02-15 17:14:33 -0300

    Add files via upload

---
+++ Relatorios/Empresa.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sistema_de_Vendas.Relatorios
+{
+    public class Empresa
+    {
+        public string nome { get; set; }
+        public string documento { get; set; }
+        public string endereco { get; set; }
+        public string bairro { get; set; }
+        public string numero { get; set; }
+        public string cidade { get; set; }
+        public string estado {  get; set; }
+        public string telefone { get; set;}
+        public string celular {  get; set; }
+        public byte[] foto {  get; set; }
+        public string email { get; set; }
+    }   
+}
