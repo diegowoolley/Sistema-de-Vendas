10fb521bfa0f37c51d6af4aa3ad746ee8e5f7cc5
diegowoolley <154614603+diegowoolley@users.noreply.github.com>
2024-03-06 11:44:20 -0300

    Add files via upload

--- Configuracoes/frmlimpartabelas.Designer.cs
+++ Configuracoes/frmlimpartabelas.Designer.cs
@@ -29,11 +29,22 @@
         private void InitializeComponent()
         {
             this.btnlimparvendas = new System.Windows.Forms.Button();
+            this.btnlimparos = new System.Windows.Forms.Button();
+            this.btnclientes = new System.Windows.Forms.Button();
+            this.btnprodutos = new System.Windows.Forms.Button();
+            this.btnfornecedor = new System.Windows.Forms.Button();
+            this.btnfuncionarios = new System.Windows.Forms.Button();
+            this.btntransportadoras = new System.Windows.Forms.Button();
+            this.btnveiculos = new System.Windows.Forms.Button();
+            this.btnservicos = new System.Windows.Forms.Button();
+            this.btncargos = new System.Windows.Forms.Button();
+            this.btnusuarios = new System.Windows.Forms.Button();
+            this.btncategoria = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // btnlimparvendas
             // 
-            this.btnlimparvendas.Location = new System.Drawing.Point(120, 30);
+            this.btnlimparvendas.Location = new System.Drawing.Point(257, 157);
             this.btnlimparvendas.Name = "btnlimparvendas";
             this.btnlimparvendas.Size = new System.Drawing.Size(138, 23);
             this.btnlimparvendas.TabIndex = 0;
@@ -41,11 +52,132 @@
             this.btnlimparvendas.UseVisualStyleBackColor = true;
             this.btnlimparvendas.Click += new System.EventHandler(this.btnlimparvendas_Click);
             // 
+            // btnlimparos
+            // 
+            this.btnlimparos.Location = new System.Drawing.Point(257, 188);
+            this.btnlimparos.Name = "btnlimparos";
+            this.btnlimparos.Size = new System.Drawing.Size(138, 23);
+            this.btnlimparos.TabIndex = 1;
+            this.btnlimparos.Text = "Limpar OS";
+            this.btnlimparos.UseVisualStyleBackColor = true;
+            this.btnlimparos.Click += new System.EventHandler(this.btnlimparos_Click);
+            // 
+            // btnclientes
+            // 
+            this.btnclientes.Location = new System.Drawing.Point(12, 12);
+            this.btnclientes.Name = "btnclientes";
+            this.btnclientes.Size = new System.Drawing.Size(185, 23);
+            this.btnclientes.TabIndex = 2;
+            this.btnclientes.Text = "Limpar cadastro de clientes";
+            this.btnclientes.UseVisualStyleBackColor = true;
+            this.btnclientes.Click += new System.EventHandler(this.btnclientes_Click);
+            // 
+            // btnprodutos
+            // 
+            this.btnprodutos.Location = new System.Drawing.Point(12, 41);
+            this.btnprodutos.Name = "btnprodutos";
+            this.btnprodutos.Size = new System.Drawing.Size(185, 23);
+            this.btnprodutos.TabIndex = 3;
+            this.btnprodutos.Text = "Limpar cadastro de Produtos";
+            this.btnprodutos.UseVisualStyleBackColor = true;
+            this.btnprodutos.Click += new System.EventHandler(this.btnprodutos_Click);
+            // 
+            // btnfornecedor
+            // 
+            this.btnfornecedor.Location = new System.Drawing.Point(12, 99);
+            this.btnfornecedor.Name = "btnfornecedor";
+            this.btnfornecedor.Size = new System.Drawing.Size(185, 23);
+            this.btnfornecedor.TabIndex = 4;
+            this.btnfornecedor.Text = "Limpar cadastro de fornecedores";
+            this.btnfornecedor.UseVisualStyleBackColor = true;
+            this.btnfornecedor.Click += new System.EventHandler(this.btnfornecedor_Click);
+            // 
+            // btnfuncionarios
+            // 
+            this.btnfuncionarios.Location = new System.Drawing.Point(12, 128);
+            this.btnfuncionarios.Name = "btnfuncionarios";
+            this.btnfuncionarios.Size = new System.Drawing.Size(185, 23);
+            this.btnfuncionarios.TabIndex = 5;
+            this.btnfuncionarios.Text = "Limpar cadastro de funcionários";
+            this.btnfuncionarios.UseVisualStyleBackColor = true;
+            this.btnfuncionarios.Click += new System.EventHandler(this.btnfuncionarios_Click);
+            // 
+            // btntransportadoras
+            // 
+            this.btntransportadoras.Location = new System.Drawing.Point(12, 157);
+            this.btntransportadoras.Name = "btntransportadoras";
+            this.btntransportadoras.Size = new System.Drawing.Size(185, 23);
+            this.btntransportadoras.TabIndex = 6;
+            this.btntransportadoras.Text = "Limpar cadastro de transportadoras";
+            this.btntransportadoras.UseVisualStyleBackColor = true;
+            this.btntransportadoras.Click += new System.EventHandler(this.btntransportadoras_Click);
+            // 
+            // btnveiculos
+            // 
+            this.btnveiculos.Location = new System.Drawing.Point(12, 186);
+            this.btnveiculos.Name = "btnveiculos";
+            this.btnveiculos.Size = new System.Drawing.Size(185, 23);
+            this.btnveiculos.TabIndex = 7;
+            this.btnveiculos.Text = "Limpar cadastro de veículos";
+            this.btnveiculos.UseVisualStyleBackColor = true;
+            this.btnveiculos.Click += new System.EventHandler(this.btnveiculos_Click);
+            // 
+            // btnservicos
+            // 
+            this.btnservicos.Location = new System.Drawing.Point(210, 12);
+            this.btnservicos.Name = "btnservicos";
+            this.btnservicos.Size = new System.Drawing.Size(185, 23);
+            this.btnservicos.TabIndex = 8;
+            this.btnservicos.Text = "Limpar cadastro de serviços";
+            this.btnservicos.UseVisualStyleBackColor = true;
+            this.btnservicos.Click += new System.EventHandler(this.btnservicos_Click);
+            // 
+            // btncargos
+            // 
+            this.btncargos.Location = new System.Drawing.Point(210, 41);
+            this.btncargos.Name = "btncargos";
+            this.btncargos.Size = new System.Drawing.Size(185, 23);
+            this.btncargos.TabIndex = 9;
+            this.btncargos.Text = "Limpar cadastro de cargos";
+            this.btncargos.UseVisualStyleBackColor = true;
+            this.btncargos.Click += new System.EventHandler(this.btncargos_Click);
+            // 
+            // btnusuarios
+            // 
+            this.btnusuarios.Location = new System.Drawing.Point(210, 70);
+            this.btnusuarios.Name = "btnusuarios";
+            this.btnusuarios.Size = new System.Drawing.Size(185, 23);
+            this.btnusuarios.TabIndex = 10;
+            this.btnusuarios.Text = "Limpar cadastro de usuários";
+            this.btnusuarios.UseVisualStyleBackColor = true;
+            this.btnusuarios.Click += new System.EventHandler(this.btnusuarios_Click);
+            // 
+            // btncategoria
+            // 
+            this.btncategoria.Location = new System.Drawing.Point(12, 70);
+            this.btncategoria.Name = "btncategoria";
+            this.btncategoria.Size = new System.Drawing.Size(185, 23);
+            this.btncategoria.TabIndex = 11;
+            this.btncategoria.Text = "Limpar cadastro de categorias";
+            this.btncategoria.UseVisualStyleBackColor = true;
+            this.btncategoria.Click += new System.EventHandler(this.btncategoria_Click);
+            // 
             // frmlimpartabelas
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(407, 225);
+            this.Controls.Add(this.btncategoria);
+            this.Controls.Add(this.btnusuarios);
+            this.Controls.Add(this.btncargos);
+            this.Controls.Add(this.btnservicos);
+            this.Controls.Add(this.btnveiculos);
+            this.Controls.Add(this.btntransportadoras);
+            this.Controls.Add(this.btnfuncionarios);
+            this.Controls.Add(this.btnfornecedor);
+            this.Controls.Add(this.btnprodutos);
+            this.Controls.Add(this.btnclientes);
+            this.Controls.Add(this.btnlimparos);
             this.Controls.Add(this.btnlimparvendas);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
             this.MaximizeBox = false;
@@ -60,5 +192,16 @@
         #endregion
 
         private System.Windows.Forms.Button btnlimparvendas;
+        private System.Windows.Forms.Button btnlimparos;
+        private System.Windows.Forms.Button btnclientes;
+        private System.Windows.Forms.Button btnprodutos;
+        private System.Windows.Forms.Button btnfornecedor;
+        private System.Windows.Forms.Button btnfuncionarios;
+        private System.Windows.Forms.Button btntransportadoras;
+        private System.Windows.Forms.Button btnveiculos;
+        private System.Windows.Forms.Button btnservicos;
+        private System.Windows.Forms.Button btncargos;
+        private System.Windows.Forms.Button btnusuarios;
+        private System.Windows.Forms.Button btncategoria;
     }
 }
--- Configuracoes/frmlimpartabelas.cs
+++ Configuracoes/frmlimpartabelas.cs
@@ -56,6 +56,323 @@ namespace Sistema_de_Vendas.Configuracoes
             }
         }
 
+        private void btnlimparos_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                DialogResult result = MessageBox.Show("Deseja realmente excluir todas as suas OS's?", "Confirmação", MessageBoxButtons.YesNo);
+                if (result == DialogResult.Yes)
+                {
+                    con.AbrirConexao();
+                    sql = "TRUNCATE TABLE cad_os";
+                    cmd = new MySqlCommand(sql, con.con);
+                    cmd.ExecuteNonQuery();
+
+                    sql = "TRUNCATE TABLE detalhes_os";
+                    cmd = new MySqlCommand(sql, con.con);
+                    cmd.ExecuteNonQuery();
+
+                    sql = "ALTER TABLE cad_os AUTO_INCREMENT = 0";
+                    cmd = new MySqlCommand(sql, con.con);
+                    cmd.ExecuteNonQuery();
+
+                    sql = "ALTER TABLE detalhes_os AUTO_INCREMENT = 0";
+                    cmd = new MySqlCommand(sql, con.con);
+                    cmd.ExecuteNonQuery();
+                    MessageBox.Show("OS's excluidas com sucesso!");
+                }
+
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btnclientes_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                DialogResult result = MessageBox.Show("Deseja realmente excluir todos os clientes?", "Confirmação", MessageBoxButtons.YesNo);
+                if (result == DialogResult.Yes)
+                {
+                    con.AbrirConexao();
+                    sql = "TRUNCATE TABLE cad_clientes";
+                    cmd = new MySqlCommand(sql, con.con);
+                    cmd.ExecuteNonQuery();
+
+                    sql = "ALTER TABLE cad_clientes AUTO_INCREMENT = 0";
+                    cmd = new MySqlCommand(sql, con.con);
+                    cmd.ExecuteNonQuery();
+
+                    con.FecharConexao();
+                    MessageBox.Show("Clientes excluidos com sucesso!");
+                }
+
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btnprodutos_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                DialogResult result = MessageBox.Show("Deseja realmente excluir todos os produtos?", "Confirmação", MessageBoxButtons.YesNo);
+                if (result == DialogResult.Yes)
+                {
+                    con.AbrirConexao();
+                    sql = "TRUNCATE TABLE cad_produtos";
+                    cmd = new MySqlCommand(sql, con.con);
+                    cmd.ExecuteNonQuery();
+
+                    sql = "ALTER TABLE cad_produtos AUTO_INCREMENT = 0";
+                    cmd = new MySqlCommand(sql, con.con);
+                    cmd.ExecuteNonQuery();
+
+                    con.FecharConexao();
+                    MessageBox.Show("Produtos excluidos com sucesso!");
+                }
+
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btncategoria_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                DialogResult result = MessageBox.Show("Deseja realmente excluir todas as categorias?", "Confirmação", MessageBoxButtons.YesNo);
+                if (result == DialogResult.Yes)
+                {
+                    con.AbrirConexao();
+                    sql = "TRUNCATE TABLE cad_categorias";
+                    cmd = new MySqlCommand(sql, con.con);
+                    cmd.ExecuteNonQuery();
+
+                    sql = "ALTER TABLE cad_categorias AUTO_INCREMENT = 0";
+                    cmd = new MySqlCommand(sql, con.con);
+                    cmd.ExecuteNonQuery();
+
+                    con.FecharConexao();
+                    MessageBox.Show("Categorias excluidas com sucesso!");
+                }
+
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btnfornecedor_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                DialogResult result = MessageBox.Show("Deseja realmente excluir todos os fornecedores?", "Confirmação", MessageBoxButtons.YesNo);
+                if (result == DialogResult.Yes)
+                {
+                    con.AbrirConexao();
+                    sql = "TRUNCATE TABLE cad_fornecedores";
+                    cmd = new MySqlCommand(sql, con.con);
+                    cmd.ExecuteNonQuery();
+
+                    sql = "ALTER TABLE cad_fornecedores AUTO_INCREMENT = 0";
+                    cmd = new MySqlCommand(sql, con.con);
+                    cmd.ExecuteNonQuery();
+
+                    con.FecharConexao();
+                    MessageBox.Show("Fornecedores excluidos com sucesso!");
+                }
+
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btnfuncionarios_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                DialogResult result = MessageBox.Show("Deseja realmente excluir todos os funcionários?", "Confirmação", MessageBoxButtons.YesNo);
+                if (result == DialogResult.Yes)
+                {
+                    con.AbrirConexao();
+                    sql = "TRUNCATE TABLE cad_funcionarios";
+                    cmd = new MySqlCommand(sql, con.con);
+                    cmd.ExecuteNonQuery();
+
+                    sql = "ALTER TABLE cad_funcionarios AUTO_INCREMENT = 0";
+                    cmd = new MySqlCommand(sql, con.con);
+                    cmd.ExecuteNonQuery();
+
+                    con.FecharConexao();
+                    MessageBox.Show("Funcionários excluidos com sucesso!");
+                }
+
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btntransportadoras_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                DialogResult result = MessageBox.Show("Deseja realmente excluir todas as transportadoras?", "Confirmação", MessageBoxButtons.YesNo);
+                if (result == DialogResult.Yes)
+                {
+                    con.AbrirConexao();
+                    sql = "TRUNCATE TABLE cad_transportadoras";
+                    cmd = new MySqlCommand(sql, con.con);
+                    cmd.ExecuteNonQuery();
+
+                    sql = "ALTER TABLE cad_transportadoras AUTO_INCREMENT = 0";
+                    cmd = new MySqlCommand(sql, con.con);
+                    cmd.ExecuteNonQuery();
+
+                    con.FecharConexao();
+                    MessageBox.Show("Transportadoras excluidas com sucesso!");
+                }
+
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btnveiculos_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                DialogResult result = MessageBox.Show("Deseja realmente excluir todos os veículos?", "Confirmação", MessageBoxButtons.YesNo);
+                if (result == DialogResult.Yes)
+                {
+                    con.AbrirConexao();
+                    sql = "TRUNCATE TABLE cad_veiculos";
+                    cmd = new MySqlCommand(sql, con.con);
+                    cmd.ExecuteNonQuery();
+
+                    sql = "ALTER TABLE cad_veiculos AUTO_INCREMENT = 0";
+                    cmd = new MySqlCommand(sql, con.con);
+                    cmd.ExecuteNonQuery();
+
+                    con.FecharConexao();
+                    MessageBox.Show("Veículos excluidos com sucesso!");
+                }
+
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btnservicos_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                DialogResult result = MessageBox.Show("Deseja realmente excluir todos os serviços?", "Confirmação", MessageBoxButtons.YesNo);
+                if (result == DialogResult.Yes)
+                {
+                    con.AbrirConexao();
+                    sql = "TRUNCATE TABLE cad_servicos";
+                    cmd = new MySqlCommand(sql, con.con);
+                    cmd.ExecuteNonQuery();
+
+                    sql = "ALTER TABLE cad_servicos AUTO_INCREMENT = 0";
+                    cmd = new MySqlCommand(sql, con.con);
+                    cmd.ExecuteNonQuery();
+
+                    con.FecharConexao();
+                    MessageBox.Show("Serviços excluidos com sucesso!");
+                }
+
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btncargos_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                DialogResult result = MessageBox.Show("Deseja realmente excluir todos os cargos?", "Confirmação", MessageBoxButtons.YesNo);
+                if (result == DialogResult.Yes)
+                {
+                    con.AbrirConexao();
+                    sql = "TRUNCATE TABLE cad_cargos";
+                    cmd = new MySqlCommand(sql, con.con);
+                    cmd.ExecuteNonQuery();
+
+                    sql = "ALTER TABLE cad_cargos AUTO_INCREMENT = 0";
+                    cmd = new MySqlCommand(sql, con.con);
+                    cmd.ExecuteNonQuery();
+
+                    con.FecharConexao();
+                    MessageBox.Show("Cargos excluidos com sucesso!");
+                }
+
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btnusuarios_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                DialogResult result = MessageBox.Show("Deseja realmente excluir todos os Usuários?", "Confirmação", MessageBoxButtons.YesNo);
+                if (result == DialogResult.Yes)
+                {
+                    con.AbrirConexao();
+                    sql = "TRUNCATE TABLE cad_usuarios";
+                    cmd = new MySqlCommand(sql, con.con);
+                    cmd.ExecuteNonQuery();
+
+                    sql = "ALTER TABLE cad_usuarios AUTO_INCREMENT = 0";
+                    cmd = new MySqlCommand(sql, con.con);
+                    cmd.ExecuteNonQuery();
+
+                    con.FecharConexao();
+                    MessageBox.Show("Usuários excluidos com sucesso!");
+                }
+
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+
         #endregion
+
+
     }
 }
